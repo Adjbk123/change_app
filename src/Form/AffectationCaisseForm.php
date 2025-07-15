58730c8f4dadc25d82de5241a84d8fec3f7b549d
Adjbk123 <adjibako123@gmail.com>
2025-07-15 12:33:27 +0100

    cor1

--- src/Form/AffectationCaisseForm.php
+++ src/Form/AffectationCaisseForm.php
@@ -6,6 +6,7 @@ use App\Entity\AffectationCaisse;
 use App\Entity\Caisse;
 use App\Entity\User;
 use App\Repository\CaisseRepository;
+use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,10 +15,12 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class AffectationCaisseForm extends AbstractType
 {
     private $caisseRepository;
+    private $userRepository;
 
-    public function __construct(CaisseRepository $caisseRepository)
+    public function __construct(CaisseRepository $caisseRepository, UserRepository $userRepository)
     {
         $this->caisseRepository = $caisseRepository;
+        $this->userRepository = $userRepository;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -26,6 +29,21 @@ class AffectationCaisseForm extends AbstractType
         $builder
             ->add('caissier', EntityType::class, [
                 'class' => User::class,
+                'choice_label' => function($user) {
+                    return $user->getNomComplet();
+                },
+                'query_builder' => function (UserRepository $ur) use ($agence) {
+                    $ids = array_map(function($u) { return $u->getId(); }, $ur->findCaissiersByAgence($agence));
+                    $qb = $ur->createQueryBuilder('u');
+                    if (count($ids) > 0) {
+                        $qb->andWhere($qb->expr()->in('u.id', ':ids'));
+                        $qb->setParameter('ids', $ids);
+                    } else {
+                        $qb->andWhere('1=0'); // Aucun résultat
+                    }
+                    return $qb;
+                },
+                'placeholder' => '-- Sélectionner un caissier --',
             ])
             ->add('caisse', EntityType::class, [
                 'class' => Caisse::class,
